6bdf2dfecceae523e5eca37df4f91a818d196746
Seongwon <wjdtjddnjs99@kaist.ac.kr>
2023-07-11 06:50:24 +0000

    진짜 도전

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var retrofitRouter = require('./routes/retrofit');
 var addressRouter = require('./routes/address');
-var authRouter = require('./lib_login/auth');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
